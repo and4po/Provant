34fec7421d7c32af7dbe87124e3beac0421fd090
and4po <94716615+and4po@users.noreply.github.com>
2023-05-26 13:44:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,27 @@ $js.addEventListener("input", update)
 $css.addEventListener("input", update)
 $html.addEventListener("input", update)
 
-function update () {
-    const html = createHtml()
-    $("iframe").setAttribute("srcdoc", html)
+function init() {
+    const { pathname } = window.location
+    const [html, css, js] = pathname.slice(1).split('%7C')
+    $html.value = window.atob(html)
+    $css.value = window.atob(css)
+    $js.value = window.atob(js)
 }
 
-const createHtml = () => {
+function update () {
     const html = $html.value
     const js = $js.value
     const css = $css.value
+
+    const hashedCode = `${window.btoa(html)}|${window.btoa(css)}|${window.btoa(js)}`
+    window.history.replaceState(null, null, `/${hashedCode}`)
+    
+    const htmlPreview = createHtml({js, html, css})
+    $("iframe").setAttribute("srcdoc", htmlPreview)
+}
+
+const createHtml = ({js, html, css}) => {
 
     return `
 <!DOCTYPE html>
@@ -37,6 +49,8 @@ const createHtml = () => {
 </html>
         `
 }
+
+init()
 
 // function download(filename, text) {
 //     var element = document.createElement('a');
